e28041c20d6cbb123153a1d2502d83f478ebb9de
TTatchell <tim.tatchell24@gmail.com>
2021-03-18 17:43:59 +1100

    Commit 5: HTML 2 + CSS 1 + JS Plan

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 //const h2 = document.createElement('h2');
 //h2.textContent = "This content added by JavaScript";
 //document.querySelector('body').appendChild(h2);
+
+//THINGS I'D LIKE TO HAVE:
+// "isMobile" "resize of rwindow"  
+
+//THINGS I NEED
+//"sticky nav for desktop"
+//Link highlighting for nav menu because I'm using div buttons through JS
+//Event handlers for the buttons
