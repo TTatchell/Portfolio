8941e13452b3a51f9aaa429103e52341b2ba22c6
TTatchell <tim.tatchell24@gmail.com>
2021-03-21 13:30:25 +1100

    Commit 7: Navigation bar

--- script.js
+++ script.js
@@ -10,3 +10,14 @@
 //"sticky nav for desktop"
 //Link highlighting for nav menu because I'm using div buttons through JS
 //Event handlers for the buttons
+
+function highlightNavButton() {
+    console.log("Hovered!");
+    document.getElementById("navBut1")["background.color"] = "red";
+    console.log("We got this far");
+    //document.getElementsByClassName("navButton").background.color = "245, 245, 245";
+    
+
+
+}
+
