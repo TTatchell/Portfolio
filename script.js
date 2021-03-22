44a4ab14d13d29f4204102d876999bad1ffe249b
TTatchell <tim.tatchell24@gmail.com>
2021-03-22 12:38:42 +1100

    Commit 12: Finished contact form

--- script.js
+++ script.js
@@ -11,64 +11,31 @@ function mouseoutNavButton(button) {
 }
 
 
-//Code to receive the contact form
+//Code to receive the contact form and export into text file
 let contactInfo = {};
 
-document.addEventListener("DOMContentLoaded", function(){
-    contactInfo.firstName = (documentgetElementById('firstName'));
-    contactInfo.lastName = (documentgetElementById('lastName'));
-    contactInfo.company = (documentgetElementById('company'));
-    contactInfo.email = (documentgetElementById('email'));
-    contactInfo.message = (documentgetElementById('message'));
-    })
-
-function checkNotEmpty(string) {
-    if (string == null || typeof value == 'undefined') return false;
-    return (value.length > 0);
-}
-
-function checkEmail (email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return regex.test(String(email).toLowerCase(0));
-}
-
-function contactInfoValidate(field, validationFunction) {
-    if (field == null) return false;
-   
-    let isFieldValid = validationFunction(field.value)
-    if (!isFieldValid) {
-    field.className = 'placeholderRed';
-    } else {
-    field.className = '';
-    }
-   
-    return isFieldValid;
-   }
-
-
-function isValid() {
-let valid = true;
-
-valid &= contactInfoValidate(contactInfo.firstName, checkNotEmpty);
-valid &= contactInfoValidate(contactInfo.lastName, checkNotEmpty);
-valid &= contactInfoValidate(contactInfo.company, checkNotEmpty);
-valid &= contactInfoValidate(contactInfo.email, checkEmail);
-valid &= contactInfoValidate(contactInfo.question, checkNotEmpty);
-return valid;
-}
-
-class User {
-    constructor(firstName, lastName, company, email, question) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.company = company;
-    this.email = email;
-    this.question = question;
-    }
-   }
 
 function sendContact() {
-    if (isValid()) {
-        let usr = new User (firstName.value, lastName.value, company.value, email.value, message.value) 
-    }
+    contactInfo.firstName = document.getElementById('firstName').value;
+    contactInfo.lastName = document.getElementById('lastName').value;
+    contactInfo.company = document.getElementById('company').value;
+    contactInfo.email = document.getElementById('email').value;
+    contactInfo.message = document.getElementById('message').value;
+
+    console.log(contactInfo)
+    
+    alert(`Hi ${contactInfo.firstName}, thanks for submitting!`);  
+
+    //export contactInfo here
+    //exportData(contactInfo);
+    
+    //Reset the input field
+    document.getElementById('firstName').value = ''
+    document.getElementById('lastName').value = ''
+    document.getElementById('company').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('message').value = ''
+
+ 
+    
 }
